ac0cd69f24eb5e66ba52e9c3514174b403d8c8a5
Shakil Hiat <s.hiat.n.1@gmail.com>
2025-07-28 19:36:56 +0600

    Update warmhole.cpp

--- warmhole.cpp
+++ warmhole.cpp
@@ -1,75 +1,56 @@
 #include <iostream>
-#include <algorithm>
-#include <utility>
-#include <cstring>
-#include <cmath>
-#include <climits>
 #include <vector>
-
+#include <climits>
+#include <cmath>
+#include <cstring>
+#include <utility>
+#include <algorithm>
 using namespace std;
-
-int hole[100][5];
-int vis[100];
-int ans;
-int n;
-
+int n, ans;
+int st_i, st_j, end_i, end_j;
+int holes[100][5];
+bool vis[100];
 int dist(int x1, int y1, int x2, int y2){
-    return abs(x1-x2) + abs(y1-y2);
-} 
+   return abs(x1-x2)+abs(y1-y2);
+}
+void wormholes(int st_i, int st_j, int cost){
+   ans = min(ans, cost + dist(st_i, st_j, end_i, end_j));
 
-void dfs(int stx, int sty, int enx, int eny, int cost){
-    ans = min(ans, cost + dist(stx, sty, enx, eny));
-
-    for(int i = 0; i<n; i++){
-        
-        if(!vis[i]){
-              
-            vis[i] = 1;
-
-            int temp = cost + dist(stx, sty, hole[i][0], hole[i][1]) + hole[i][4];
-            dfs(hole[i][2], hole[i][3], enx, eny, temp);
-
-            temp = cost + dist(stx, sty, hole[i][2], hole[i][3]) + hole[i][4];
-            dfs(hole[i][0], hole[i][1], enx, eny, temp);
-
-            vis[i] = false;
-
-        }
-    }
-
+   for(int i = 0;i<n; i++){
+      if(!vis[i]){
+         vis[i] = true;
+         int temp = cost + dist(st_i, st_j, holes[i][0], holes[i][1]) + holes[i][4];
+         wormholes(holes[i][2], holes[i][3], temp);
+         
+         temp = cost + dist(st_i, st_j, holes[i][2], holes[i][3]) + holes[i][4];
+         wormholes(holes[i][0], holes[i][1], temp);
+         vis[i] = false;
+      }
+   }
 }
 
 
 void solve(){
+   cin >> n;
+   cin >> st_i >> st_j >> end_i >> end_j;
+   for(int i= 0; i<n; i++){
+      for(int j=0; j<5; j++){
+         cin >> holes[i][j];
+      }      
+   }
+   memset(vis, 0, sizeof vis);
 
-    cin >> n;
-    int start_x, start_y, dest_x, dest_y;
-    cin >> start_x >> start_y >> dest_x >> dest_y;
-
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j<5; j++){
-            cin >> hole[i][j];
-        } 
-    }
-
-    memset(vis, 0, sizeof vis);
-
-    ans = INT_MAX;
-
-    dfs(start_x, start_y, dest_x, dest_y, 0);
-
-    cout << ans << endl;
-
-
+   ans = INT_MAX;
+   wormholes(st_i, st_j, 0);
+   cout << ans << endl;
 }
 
 
 int main(){
-
-    int cas;
-    cin >> cas;
-    while(cas--){
-        solve();
-    }
-    return 0;
+   int cas;
+   cin >> cas;
+   while(cas --){
+      solve();
+   }
+   return 0;
 }
